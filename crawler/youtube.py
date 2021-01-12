8442b0417b3d6ce401430067fd75a30cd9165c62
fbghgus123 <fbghgus123@naver.com>
2021-01-12 12:09:10 +0900

    youtube.py fix

--- crawler/youtube.py
+++ crawler/youtube.py
@@ -7,7 +7,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
 # Add modules in common/functions.py
 sys.path.append(os.getcwd())
 
@@ -21,7 +20,7 @@ visible = EC.visibility_of_element_located
 time.sleep(1)
 
 search = driver.find_element_by_xpath('//input[@id="search"]')
-video = "gracie abrams 21"
+video = 'gracie abrams 21'
 search.send_keys(video)
 time.sleep(1)
 
@@ -31,36 +30,33 @@ driver.get("https://www.youtube.com/results?search_query=" + str(video))
 
 wait.until(visible((By.ID, "video-title")))
 driver.find_element_by_id("video-title").click()
-time.sleep(5)
+time.sleep(3.0)
 
-num_of_pagedowns = 2
-while num_of_pagedowns:
-    driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
-    time.sleep(3.0)
-    new_page_height = driver.execute_script("return document.documentElement.scrollHeight")
-    num_of_pagedowns -= 1
+driver.execute_script("window.scrollTo(0,100);") #document.documentElement.scrollHeight-500
+time.sleep(3.0)
+
+
 html_source = driver.page_source
-
 driver.close()
 
-soup = BeautifulSoup(html_source, "lxml")
+soup = BeautifulSoup(html_source, 'lxml')
 
-youtube_user_IDs = soup.select("div#header-author > a > span")
-youtube_comments = soup.select("yt-formatted-string#content-text")
+youtube_user_IDs = soup.select('div#header-author > a > span')
+youtube_comments = soup.select('yt-formatted-string#content-text')
 
 str_youtube_userIDs = []
 str_youtube_comments = []
 for i in range(len(youtube_user_IDs)):
     str_tmp = str(youtube_user_IDs[i].text)
-    str_tmp = str_tmp.replace("\n", "")
-    str_tmp = str_tmp.replace("\t", "")
-    str_tmp = str_tmp.replace(" ", "")
+    str_tmp = str_tmp.replace('\n', '')
+    str_tmp = str_tmp.replace('\t', '')
+    str_tmp = str_tmp.replace(' ','')
     str_youtube_userIDs.append(str_tmp)
 
     str_tmp = str(youtube_comments[i].text)
-    str_tmp = str_tmp.replace("\n", "")
-    str_tmp = str_tmp.replace("\t", "")
-    str_tmp = str_tmp.replace(" ", "")
+    str_tmp = str_tmp.replace('\n', '')
+    str_tmp = str_tmp.replace('\t', '')
+    str_tmp = str_tmp.replace(' ', '')
 
     str_youtube_comments.append(str_tmp)
 
@@ -69,7 +65,6 @@ for i in range(len(youtube_user_IDs)):
 
 import pandas as pd
 
-
-pd_data = {"ID": str_youtube_userIDs, "Comment": str_youtube_comments}
+pd_data = {"ID":str_youtube_userIDs, "Comment":str_youtube_comments}
 youtube_pd = pd.DataFrame(pd_data)
 print(youtube_pd)
