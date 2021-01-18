91a74ca307d586d44b7717c22d4262785e709f90
fbghgus123 <fbghgus123@naver.com>
2021-01-18 17:15:23 +0900

    Fix problem number 3 in #18
    need more efficient method

--- crawler/youtube_module.py
+++ crawler/youtube_module.py
@@ -11,6 +11,19 @@ from common.functions import ChromeDriver
 # Add modules in common/functions.py  
 sys.path.append(os.getcwd())
 
+def inspect_comment_respone(last_html_source, new_html_source):
+    soup = BeautifulSoup(last_html_source, 'lxml')
+    last = soup.select('yt-formatted-string#content-text')
+
+
+    soup = BeautifulSoup(new_html_source, 'lxml')
+    new = soup.select('yt-formatted-string#content-text')
+
+    if last == new:
+        return 1
+    else:
+        return 0
+
 def find_comment(html_source):
     soup = BeautifulSoup(html_source, 'lxml')
 
@@ -64,17 +77,15 @@ def search_music(video):
     # 스크롤 내려서 댓글 불러오기
     driver.execute_script("window.scrollTo(0,500);")
     time.sleep(3) # 2초 설정시 못가져오는 경우 있음
-    
-    last_page_height = driver.execute_script("return document.documentElement.scrollHeight")
-    
+
     maximum_comment = 0
     while True:
+        last_html_source = driver.page_source
         driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
         time.sleep(3) # 2초 설정시 못가져오는 경우 있음
-        new_page_height = driver.execute_script("return document.documentElement.scrollHeight")
-        if new_page_height == last_page_height or maximum_comment > 2: # maximum_comment: 댓글 최대 개수 설정
+        new_html_source = driver.page_source
+        if maximum_comment > 2 or inspect_comment_respone(last_html_source,new_html_source): # maximum_comment: 댓글 최대 개수 설정
             break
-        last_page_height = new_page_height
         maximum_comment += 1
 
     # html 저장 후 드라이버 닫기
