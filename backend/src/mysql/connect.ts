637e00c5068b978caaa2519d5689d19e4f1edb6f
wnstj2007 <wnstj2096@gmail.com>
2021-01-21 10:42:32 +0900

    Modify variable name

--- backend/src/mysql/connect.ts
+++ backend/src/mysql/connect.ts
@@ -1,12 +1,12 @@
-import conn from './config/config'
+import connection from './config/config'
 
-conn.connect()
+connection.connect()
 
-conn.query('select * from users;', (err: Error, rows: any, cols: any) => {
+connection.query('select * from users;', (err: Error, rows: any, cols: any) => {
   if (err) {
     throw err
   }
   console.log('user info : ', rows)
 })
 
-conn.end()
+connection.end()
