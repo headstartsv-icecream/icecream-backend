3b0936226fa609ea29202fdf3eb6aaeee6189dcf
wnstj2007 <wnstj2096@gmail.com>
2021-01-21 15:13:43 +0900

    Modify mysql container name

--- mysql/mysql-start.sh
+++ mysql/mysql-start.sh
@@ -1,3 +1,3 @@
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
-docker run -v ./mysql/db:/etc/mysql/conf.d -v ./mysql/db:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=1234 -p 3306:3306 -d mysql:latest
+docker run --name mysql -v ./mysql/db:/etc/mysql/conf.d -v ./mysql/db:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=1234 -p 3306:3306 -d mysql:latest
