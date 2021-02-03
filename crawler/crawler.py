5f78a7e4100c619cd682c6b58ceed91a93508c37
fbghgus123 <fbghgus123@naver.com>
2021-02-03 21:45:25 +0900

    change to connect mysql address

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -17,7 +17,7 @@ from sqlalchemy import create_engine
 pymysql.install_as_MySQLdb()
 import MySQLdb
 
-engine = create_engine("mysql://root:"+"1234"+"@localhost/app", encoding='utf-8')
+engine = create_engine("mysql://root:"+"1234"+"@34.64.111.47/app", encoding='utf-8')
 # "mysql://아이디:"+"비밀번호"+"@mysql주소:포트/DB이름"
 
 connect = engine.connect()
@@ -29,7 +29,7 @@ data3.to_sql(name="music", con=engine, if_exists="append", index=False)
 
 connect.close()
 
-conn = pymysql.connect(host='localhost', user='root', password='1234',
+conn = pymysql.connect(host='34.64.111.47', user='root', password='1234',
                        db='app', charset='utf8')
  
 # Connection 으로부터 Dictoionary Cursor 생성
